86e727fe2e427b388934a0ad60fad0513cba97af
Bolshakov Nikita <bolevgnik21@gmail.com>
2024-06-11 00:16:16 +0300

    hot fixes

--- src/masks.py
+++ src/masks.py
@@ -1,3 +1,10 @@
+from widget import card_number_input
+from widget import account_input
+from widget import card_type
+from widget import get_data
+from widget import user_date_input
+
+
 def get_mask_card_number(card_number: str) -> str:
     """функция возвращает маску карты"""
     masked_card_number = card_number[:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
@@ -10,10 +17,10 @@ def get_mask_account(mask_account: str) -> str:
     return masked_account
 
 
-"""Вводим данные карты и аккаунта"""
-card_number_input: str = input()
-account_input: str = input()
-
-print(get_mask_card_number(card_number_input))
-print(get_mask_account(account_input))
-
+"""Проверяем что у нас на входе: номер карты или номер счета и выводим в зашифрованном виде """
+if card_type != "Счет":
+    print(card_type, get_mask_card_number(card_number_input))
+elif card_type == "Счет":
+    print(card_type, get_mask_account(account_input))
+"""Выводим дату"""
+print(get_data(user_date_input))
