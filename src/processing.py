1ea6aaa26a0cf2d836c38014edf72c9e0a25328e
arkadii9423 <bolevgnik21@gmail.com>
2024-06-13 21:00:38 +0300

    making docstrings

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,5 @@
 def filter_by_state(unfiltered_state_input: list) -> str:
+    """Функция возвращает операции, отфильтрованные на две группы"""
     dictionary_1 = []
     dictionary_2 = []
     for item in unfiltered_state_input:
@@ -11,5 +12,6 @@ def filter_by_state(unfiltered_state_input: list) -> str:
 
 
 def sort_by_date(unsorted_date_input: list[dict[str]]) -> list[dict[str]]:
+    """Функция возвращает отсортированные транзакции по дате"""
     sorted_data = sorted(unsorted_date_input, key=lambda x: x["date"], reverse=True)
     return sorted_data
