92d609a603b3bd6f798164fbd836cb11b5ddced7
arkadii9423 <bolevgnik21@gmail.com>
2024-06-17 14:11:55 +0300

    fixes

--- src/processing.py
+++ src/processing.py
@@ -1,17 +1,14 @@
-def filter_by_state(unfiltered_state_input: list) -> str:
-    """отфильтрованные на две группы"""
-    dictionary_1 = []
-    dictionary_2 = []
+def filter_by_state(unfiltered_state_input, state: list) -> list:
+    """Функция фильтрует список словарей по значению ключа state и возвращает новый список"""
+    dictionary = []
     for item in unfiltered_state_input:
         for key, value in item.items():
-            if value == "EXECUTED":
-                dictionary_1.append(item)
-            elif value == "CANCELED":
-                dictionary_2.append(item)
-    return f"{dictionary_1}\n{dictionary_2}"
+            if value == state:
+                dictionary.append(item)
+    return dictionary
 
 
 def sort_by_date(unsorted_date_input: list[dict[str]]) -> list[dict[str]]:
-    """Функция возвращает отсортированные транзакции по дате"""
+    """Функция филтрует список транзакций по датам и возвращает их же в отсортированном виде"""
     sorted_data = sorted(unsorted_date_input, key=lambda x: x["date"], reverse=True)
     return sorted_data
