5c97b9e4eec21c040b8fba879950d7017486730e
arkadii9423 <bolevgnik21@gmail.com>
2024-06-19 00:45:09 +0300

    isort

--- src/processing.py
+++ src/processing.py
@@ -12,9 +12,3 @@ def sort_by_date(unsorted_date_input: list) -> list:
     """Функция филтрует список транзакций по датам и возвращает их же в отсортированном виде"""
     sorted_data = sorted(unsorted_date_input, key=lambda x: x["date"], reverse=True)
     return sorted_data
-
-
-print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-      {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-      {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], "CANCELED"))
