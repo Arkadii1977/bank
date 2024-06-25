78a26312965f5064a9ea35a30c5851213aa57922
arkadii9423 <bolevgnik21@gmail.com>
2024-06-25 17:43:51 +0300

    adding lost func and docstings

--- src/generators.py
+++ src/generators.py
@@ -80,6 +80,7 @@ transactions = (
 
 
 def filter_by_currency(money_move, currency):
+    """Функция принимает список словарей и возвращает по очереди операции, в которых указана заданная валюта"""
     for action in money_move:
         if action.get("operationAmount", {}).get("currency", {}).get("code") == currency:
             yield action
@@ -92,6 +93,7 @@ for transaction in range(2):
 
 
 def transaction_descriptions(money_move):
+    """Функция принимает список словарей и возвращает описание каждой операции по очереди"""
     for action in money_move:
         yield action['description']
 
@@ -100,3 +102,15 @@ descriptions = transaction_descriptions(transactions)
 
 for transaction in range(5):
     print(next(descriptions))
+
+
+def card_number_generator(start, stop):
+    """Функция генерирует номера карт в заданном диапазоне"""
+    for num in range(start, stop+1):
+        card_num = '{:016d}'.format(num)
+        formatted_card_num = ' '.join([card_num[i:i+4] for i in range(0, len(card_num), 4)])
+        yield formatted_card_num
+
+
+for card_number in card_number_generator(1, 2000):
+    print(card_number)
